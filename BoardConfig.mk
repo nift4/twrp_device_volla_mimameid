355125962ca6e72c7908ed624e6cdc39dfe61a99
Synesio Neto <synesio.neto@outlook.com>
2018-01-10 12:30:10 +0000

    mimameid: Enable NTFS-3G in TWRP Builds
    Change-Id: I8c52f22fd868a71ce7e5c92583f85ccb741f8c9c
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,6 +127,7 @@ TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 128
 TW_EXCLUDE_APEX := true
+TW_INCLUDE_NTFS_3G := true
 
 # Debug
 TWRP_INCLUDE_LOGCAT := true
