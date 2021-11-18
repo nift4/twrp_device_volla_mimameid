06a09d4347ff3a7c4ad04ad150427121a807612b
Aseem Athale <athaleaseem@gmail.com>
2021-11-18 13:55:37 +0530

    mimameid: Remove TW_USE_TOOLBOX
    Toolbox has long been the default in TWRP, this flag is no longer needed
    or used. In fact, it is automatically assumed true, unless you
    specifically specify TW_FORCE_USE_BUSYBOX.
    
    Change-Id: If19d67f9377481e522a488364bd881c5fb389611
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,7 +121,6 @@ TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_USE_TOOLBOX := true
 TW_INCLUDE_REPACKTOOLS := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 255
