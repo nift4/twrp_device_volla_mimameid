16a193498de58ddb1c1100e6a127b2c9857f4363
Albert I <kras@raphielgang.org>
2021-11-25 11:48:36 +0800

    begonia: Disable TWRP's loading of APEX modules
    This only causes overhead as we don't really need to load anything for any
    essential functions to work.
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>
    Change-Id: I897dfa27b17034a78e9b2576cf35e42866b5c19a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,6 +125,7 @@ TW_INCLUDE_REPACKTOOLS := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 128
+TW_EXCLUDE_APEX := true
 
 # Debug
 TWRP_INCLUDE_LOGCAT := true
