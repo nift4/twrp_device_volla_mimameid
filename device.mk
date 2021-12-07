7e32e4a6a43bf332eb1898eec8b5afecdb3dcc6a
Erfan Abdi <erfangplus@gmail.com>
2021-12-07 20:58:35 +0330

    mimameid: Import and build mtk bootctrl

--- device.mk
+++ device.mk
@@ -24,11 +24,7 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
-
-PRODUCT_PACKAGES += \
-    bootctrl.mt6768
+    android.hardware.boot@1.1-mtkimpl.recovery
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
